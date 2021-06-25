86c743e050ab805ef11bfbf30ac93db96ee82594
pa2001 <86311707+pa2001@users.noreply.github.com>
2021-06-26 03:19:42 +0530

    Add files via upload

---
+++ weather app using javascript/main.js
@@ -0,0 +1,29 @@
+
+var inputval = document.querySelector('#cityinput')
+var btn = document.querySelector('#add');
+var city = document.querySelector('#cityoutput')
+var descrip = document.querySelector('#description')
+var temp = document.querySelector('#temp')
+var wind = document.querySelector('#wind')
+apik = "1f65397a78bc9c82ff296b5ac3c2b927"
+function convertion(val){
+    return (val - 273).toFixed(2)
+}
+    btn.addEventListener('click', function(){
+        fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputval.value+'&appid='+apik)
+        .then(res => res.json())
+        .then(data => {
+            var nameval = data['name']
+            var descrip = data['weather']['0']['description']
+            var tempature = data['main']['temp']
+            var wndspd = data['wind']['speed']
+            var image=data['weather']['0']['icon']
+            city.innerHTML=`City: ${nameval}`
+            temp.innerHTML = `Temperature: ${ convertion(tempature)} C`
+            description.innerHTML = `Conditions: ${descrip}`
+            wind.innerHTML = `Wind Speed: ${wndspd} km/h`
+            img.innerHTML=`<img src="img/${image}.png"/>`;
+        })
+        .catch(err => alert('You entered Wrong city name'))
+    })
+
